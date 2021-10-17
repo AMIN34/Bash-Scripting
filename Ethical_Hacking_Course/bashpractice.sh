c4db75c7628f418b8e26c094eab6514dfb67e3a4
MUNSHI AMINUR RASHID <aminurrashid224@gmail.com>
2021-10-17 09:46:23 +0000

    New Directory

---
+++ Ethical_Hacking_Course/bashpractice.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+read -p "Enter Username: " user
+read -sp "Enter Password: " passwd
+
+echo "Hello $user, your password is $passwd"
---
+++ Ethical_Hacking_Course/function.sh
@@ -0,0 +1,7 @@
+number = $1
+
+passArgument () {
+  echo "User provided number is: $number"
+}
+
+passArgument
