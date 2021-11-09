dbd8d8b8dda7aefd12dcf8831fa247ab2840e0ea
MUNSHI AMINUR RASHID <aminurrashid224@gmail.com>
2021-11-09 06:07:08 +0000

    new pattern using tput cup

---
+++ Day4/pattern4.sh
@@ -0,0 +1,27 @@
+read -p "Enter no. of rows: " rows
+clear
+i=0
+j=$rows
+while [ $i -lt $rows ]
+do
+	if [ $i -eq $((rows - 1)) ]
+        then
+                c=0
+                j=1
+                while [ $c -lt $rows ]
+                do
+                        tput cup $i $j
+                        echo -n "*"
+                        j=$((j+2))
+                        c=$((c+1))
+                done
+				echo
+                break
+        fi
+	tput cup $i $((j-i))
+	echo -n "*"
+	tput cup $i $((j+i))
+	echo -n "*" 
+	i=$((i+1))
+	echo
+done
